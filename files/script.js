793ca8824c1bb7ef99aab1d864aea1e5465f1172
francescosche <schettino.francesco@live.it>
2017-12-19 18:07:34 +0100

    Apparizione dinamica pannello
    - Modificati i file delle sale, che ora mostrano posti occupati differenti in base al film che si sceglie
    
    - Accorpata la funzione che mostra o nasconde le voci del pannello, in base alla voce del menù che si clicca
    
    - Aggunte le immagini di "promo" che vengono mostrate quando si deve confermare o annullare la prenotazione
    
    - Rimpiazzate le locandine in bassa definizione con quelle in altissima definizione
    
    - Realizzata funzione "acquisto()" che mostra il messaggio di successo o insuccesso in base alla scelta dell'utente di acquistare o meno i biglietti selezionati
    
    - Riordinate le funzioni nel file "script.js"
    
    - Realizzata la funzione crea_link_chiusura(), che viene richiamata nella funzione crea_overlay(), la quale aggiunge un link alla <div> ".overlay" che, quando cliccato, chiude qualsiasi popup e l'overlay, tornando alla schermata "base"

--- files/script.js
+++ files/script.js
@@ -1,7 +1,7 @@
-function mostra_popup() {
-    setTimeout(function(){
-        $('.popup').css("opacity", "1");
-    },100);
+function titolo(id) {
+    $.get("film/"+id+"/titolo.txt", function(data) {
+        return data;
+    });
 }
 
 function apri(pagina, timeout=1) {
@@ -12,38 +12,58 @@ function apri(pagina, timeout=1) {
     });
 }
 
+function mostra_popup() {
+    setTimeout(function(){
+        $('.popup').css("opacity", "1");
+    },100);
+}
+
+function chiudi_popup(overlay=false) {
+    $('.popup').css("opacity","0");
+    setTimeout(function() {
+        $('.overlay').html('');
+        crea_link_chiusura();
+    }, 400);
+    if(overlay) chiudi_overlay(100);
+}
+
 function crea_overlay() {
     $('body').css('overflow','hidden');
     $('body').append('<div class="overlay"></div>');
     setTimeout(function(){
         $('.overlay').css("background-color", "rgba(0,0,0,0.7)");
     },10);
+    crea_link_chiusura();
     localStorage.ora = null;
     localStorage.giorno = null;
     localStorage.prenotazione = null;
 }
 
-function titolo(id) {
-    $.get("film/"+id+"/titolo.txt", function(data) {
-        return data;
-    });
-}
-
-function chiudi_popup(overlay=false) {
-    $('.popup').css("opacity","0");
-    setTimeout(function() {
-        $('.overlay').html("");
-    }, 400);
-    if(overlay) chiudi_overlay(100);
-}
-
-function chiudi_overlay(to) {
-    timeout = to != null ? to : 1;
+function chiudi_overlay(timeout=1) {
     setTimeout(function() {
         $('.overlay').css("opacity","0");
         setTimeout(function() {
             $('.overlay').remove();
         }, 400);
-    }, to);
+    }, timeout);
     $('body').css("overflow", "auto");
+}
+
+function crea_link_chiusura() {
+    $('.overlay').html('<a class="chiudi_overlay"></a>');
+    $('.overlay a').bind("click", function() {
+        $('#pannello').css("left", "130vw");
+        chiudi_overlay(50);
+    });
+}
+
+function panel(what) {
+    crea_overlay();
+    $.get("cinema.html",function(data) {
+        $('.overlay').append(data);
+        setTimeout(function() {
+            $('.'+what).addClass("open");
+            $('#pannello').css("left", "70vw");
+        }, 50);
+    });
 }
